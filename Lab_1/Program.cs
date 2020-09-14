c84bbace766315ebd972b076f9aa6441b18554c7
Andrey <62840086+TomGnill@users.noreply.github.com>
2020-09-14 13:58:56 +0300

    14.09.20

---
+++ Lab_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace CFG_mng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char open_cat = '[', close_cat = ']', value_indecator = '=';
+            
+
+            
+            string catlog = @"C:\Users\Андрейка\source\repos\OOP_LABS_AD\Lab_1\config_test.ini";
+            using (StreamReader sr = new StreamReader(catlog, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    for(int i=0; i<line.length; i ++)
+                    Console.WriteLine(line);
+                }
+            }
+         
+
+        }
+    }
+}
---
+++ Lab_1/functions.h
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CFG_mng
+{
+    class functions
+    {
+
+    }
+
+    class obr
+    {
+        List<var> parametr
+
+    };
+}
