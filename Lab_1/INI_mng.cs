d6975000b3745fd1db3c7b7042e0d442ba7dad37
Andrey <62840086+TomGnill@users.noreply.github.com>
2020-09-30 20:22:32 +0300

    work in progress(string reader)

--- Lab_1/INI_mng.cs
+++ Lab_1/INI_mng.cs
@@ -21,7 +21,7 @@ namespace CFG_mng
 
         }
 
-       public string value_of_parametr
+        public string value_of_parametr
         {
             get { return value_of_parametr; }
             set { value_of_parametr = value; }
@@ -38,6 +38,7 @@ namespace CFG_mng
             get { return name_of_sector; }
             set { name_of_sector = value; }
         }
+
         enum func_num
         {
             open_file = 1,
@@ -56,23 +57,29 @@ namespace CFG_mng
                     break;
                 case func_num.par_list:
                     printList();
-                        ;
+                    ;
                     break;
                 case func_num.int_search:
                     searchParam();
-                        ;break;
+                    ;
+                    break;
                 case func_num.exit:
                     exit_interactive();
-                        ;break;
-                default: Console.WriteLine(" Вы выбрали несуществующее действие!");
+                    ;
+                    break;
+                default:
+                    Console.WriteLine(" Вы выбрали несуществующее действие!");
                     break;
 
             }
+        }
+
+
 
         public struct param_obj
         {
             public string param_name;
-            public string param_value;
+            public string param_sector;
         }
 
         public ini_open(string path)
@@ -87,22 +94,149 @@ namespace CFG_mng
             var record_param = new param_obj
             {
                 param_name = name_of_parametr,
-                param_value = value_of_parametr
+                param_sector = name_of_sector
             };
-            param_list.Add(record_param, value_of_parametr);
+            param_list.Add(record_param, name_of_sector);
         }
 
         public void searchParam()
         {
-            foreach (var(key, value) in param_list)
+            foreach (var (key, value) in param_list)
             {
-                Console.WriteLine(key.param_name);
+                Console.WriteLine(key.param_name + " - " + key.param_sector + " - " + value);
             }
 
         }
-      public void ini_reader()
+
+        public int string_reader(string file_string)
         {
-           
+            if (file_string.Trim().Length == 0)
+                return 0;
+            if (file_string.StartsWith("[") && file_string.EndsWith("]"))
+            {
+
+            }
+            if 
+
+
+        }
+        public void ini_reader()
+        {
+
+            var fileInfo = new FileInfo(Pathfile);
+            if (fileInfo.Exists && fileInfo.Extension == ".ini")
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(catlog, System.Text.Encoding.Default))
+                    {
+                        string textLine;
+                        while ((textLine = sr.ReadLine()) != null)
+                        {
+                            numstring++;
+                            if (string_reader(file_string) == 1)
+                            {
+                                new_param();
+                            }
+
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message + " in " + Pathfile);
+                    throw;
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException("Место расположения недоступно " + Pathfile);
+            }
+        }
+    }
+
+    /*struct cfg_obj
+        {
+            public string cfg_name;
+            public string cfg_value;
+            public string cfg_sector;
+
+            public cfg_obj(string gl_name, string name, string value)
+            {
+                cfg_sector = gl_name;
+                cfg_name = name;
+                cfg_value = value;
+            }
+
+            public void GetInfo()
+            {
+                Console.WriteLine($"Раздел: {cfg_sector} параметр: {cfg_name} значение параметра: {cfg_value}");
+            }
+
+            public void GetValue()
+            {
+                Console.WriteLine($"Значение параметра: {cfg_value}");
+            }
+
+
+        };
+    char open_cat = '[', close_cat = ']', value_indecator = '=';
+        private string Pathfile { get; }
+
+        UInt16 numstring
+        {
+            get { return numstring; }
+
+            set { numstring = value; }
+        }
+
+        private string value_of_parametr
+        {
+            get { return value_of_parametr; }
+
+            set { value_of_parametr = value; }
+        }
+
+     private string name_of_parametr
+        {
+            get { return name_of_parametr; }
+
+            set { name_of_parametr = value; }
+        }
+
+     private string name_of_sector
+     {
+        get
+         {
+             return name_of_sector;
+         }
+        set
+         {
+             name_of_sector = value;
+         }
+     }
+
+      string catlog = @"C:\Users\Андрейка\source\repos\OOP_LABS_AD\Lab_1\config_test.ini";
+        public interactive_parser(string path)
+        {
+            Pathfile = path;
+            numstring = 0;
+            ReadINI();
+        }
+
+        void NewRec()
+        {
+            var RecP = new cfg_obj
+            {
+                cfg_value = value_of_parametr,
+                cfg_name = name_of_parametr,
+                cfg_sector = name_of_sector
+            };
+
+        }
+        public void ReadINI()
+
+        {
             var fileInfo = new FileInfo(Pathfile);
             if (fileInfo.Exists && fileInfo.Extension == ".ini")
             {
@@ -130,121 +264,10 @@ namespace CFG_mng
                 throw new FileNotFoundException("Место расположения недоступно " + Pathfile);
             }
         }
-        }
+        public bool objectString(string textline)
+        {
 
-        /*struct cfg_obj
-            {
-                public string cfg_name;
-                public string cfg_value;
-                public string cfg_sector;
-    
-                public cfg_obj(string gl_name, string name, string value)
-                {
-                    cfg_sector = gl_name;
-                    cfg_name = name;
-                    cfg_value = value;
-                }
-    
-                public void GetInfo()
-                {
-                    Console.WriteLine($"Раздел: {cfg_sector} параметр: {cfg_name} значение параметра: {cfg_value}");
-                }
-    
-                public void GetValue()
-                {
-                    Console.WriteLine($"Значение параметра: {cfg_value}");
-                }
-    
-    
-            };
-        char open_cat = '[', close_cat = ']', value_indecator = '=';
-            private string Pathfile { get; }
-    
-            UInt16 numstring
-            {
-                get { return numstring; }
-    
-                set { numstring = value; }
-            }
-    
-            private string value_of_parametr
-            {
-                get { return value_of_parametr; }
-    
-                set { value_of_parametr = value; }
-            }
-    
-         private string name_of_parametr
-            {
-                get { return name_of_parametr; }
-    
-                set { name_of_parametr = value; }
-            }
-    
-         private string name_of_sector
-         {
-            get
-             {
-                 return name_of_sector;
-             }
-            set
-             {
-                 name_of_sector = value;
-             }
-         }
-    
-          string catlog = @"C:\Users\Андрейка\source\repos\OOP_LABS_AD\Lab_1\config_test.ini";
-            public interactive_parser(string path)
-            {
-                Pathfile = path;
-                numstring = 0;
-                ReadINI();
-            }
-    
-            void NewRec()
-            {
-                var RecP = new cfg_obj
-                {
-                    cfg_value = value_of_parametr,
-                    cfg_name = name_of_parametr,
-                    cfg_sector = name_of_sector
-                };
-    
-            }
-            public void ReadINI()
-    
-            {
-                var fileInfo = new FileInfo(Pathfile);
-                if (fileInfo.Exists && fileInfo.Extension == ".ini")
-                {
-                    try
-                    {
-                        using (StreamReader sr = new StreamReader(catlog, System.Text.Encoding.Default))
-                        {
-                            string textLine;
-                            while ((textLine = sr.ReadLine()) != null))
-                            {
-                                numstring++;
-                                if///
-    
-                            }
-                        }
-                    }
-                    catch (Exception exception)
-                    {
-                        Console.WriteLine(exception.Message + " in " + Pathfile);
-                        throw;
-                    }
-                }
-                else
-                {
-                    throw new FileNotFoundException("Место расположения недоступно " + Pathfile);
-                }
-            }
-            public bool objectString(string textline)
-            {
-    
-    
-            } */
-    }
+
+        } */
 }
+
