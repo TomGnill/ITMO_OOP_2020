5e63dc6b6a0dc7eabb21b53f9fe844f57688d12d
Andrey <62840086+TomGnill@users.noreply.github.com>
2020-10-03 18:00:59 +0300

    Add files via upload

--- Lab_1/INI_mng.cs
+++ Lab_1/INI_mng.cs
@@ -12,38 +12,18 @@ namespace CFG_mng
         string catlog = @"C:\Users\Андрейка\source\repos\OOP_LABS_AD\Lab_1\config_test.ini";
         public string Pathfile { get; set; }
 
-        public UInt16 numstring
-        {
-            get { return numstring; }
-            set { numstring = value; }
-
-        }
-
-        public string value_of_parametr
-        {
-            get { return value_of_parametr; }
-            set { value_of_parametr = value; }
-        }
-
-        public string name_of_parametr
-        {
-            get { return name_of_parametr; }
-            set { name_of_parametr = value; }
-        }
-
-        public string name_of_sector
-        {
-            get { return name_of_sector; }
-            set { name_of_sector = value; }
-        }
-
-       public void ini_open(string path)
-        {
+        private UInt16 numstring;
+        public string value_of_parametr;
+        public string name_of_parametr;
+        public string name_of_sector;
+        public  interactive_parser(string path)
+       {
+            name_of_sector = null;
             Pathfile = path;
             numstring = 0;
             ini_reader();
-        }
-        enum func_num
+       }
+        /*   enum func_num
         {
             open_file = 1,
             par_list,
@@ -68,7 +48,7 @@ namespace CFG_mng
                     ;
                     break;
                 case func_num.exit:
-                    exit_interactive(y_n);
+                    exit_interactive();
                     ;
                     break;
                 default:
@@ -78,39 +58,44 @@ namespace CFG_mng
             }
         }
 
-         static public void exit_interactive(char y_n)
-        {
+         static public void exit_interactive()
+         {
+
+             string y_n = Console.ReadLine();
+
             switch (y_n)
             {
-                case 'y': Environment.Exit(0);break;
-                case 'n':inter_menu(func_num.open_file);
+                case ("yes"): Environment.Exit(0);break;
+                case ("no") :inter_menu(func_num.open_file);
                     break;
-                default: Console.WriteLine("Y/N?");
-                    exit_interactive(y_n);break;
+                default: Console.WriteLine("Yes/No?");
+                    exit_interactive();break;
             }
             
-        }
+        }*/
 
         public struct param_obj
         {
             public string param_name;
             public string param_sector;
+            public string param_value;
         }
         void new_param()
         {
             var record_param = new param_obj
             {
                 param_name = name_of_parametr,
-                param_sector = name_of_sector
+                param_sector = name_of_sector,
+                param_value = value_of_parametr
             };
             param_list.Add(record_param, name_of_sector);
         }
 
-        public void searchParam()
+        public void ParamList() 
         {
             foreach (var (key, value) in param_list)
             {
-                Console.WriteLine(key.param_name + " - " + key.param_sector + " - " + value);
+                Console.WriteLine(key.param_name + " - " + key.param_sector + " - " + key.param_value);
             }
 
         }
@@ -172,7 +157,8 @@ namespace CFG_mng
                             }
 
                         }
-                    }
+                        ParamList();
+                    } 
                 }
                 catch (Exception exception)
                 {
---
+++ Lab_1/mainfile.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CFG_mng
+{   public class programm
+    { 
+        static void Main()
+        {
+            interactive_parser parser1 = new interactive_parser("C:/Users/Андрейка/source/repos/OOP_LABS_AD/Lab_1/config_test.ini");
+        }
+
+    }
+}
