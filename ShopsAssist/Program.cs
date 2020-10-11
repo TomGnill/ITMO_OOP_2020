6f757eed8350108f02a1238b5c833c97ab86704c
s.drun@yandex.ru <s.drun@yandex.ru>
2020-10-12 01:10:40 +0300

    start lab2

---
+++ ShopsAssist/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ShopsAssist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+   public class Shop
+    {
+        struct Magazine
+        {
+            private int ShopID;
+            FormPriceList(string catlog);
+        }
+
+       public struct product
+       {
+           private int ProductID;
+           private int quantity;
+       }
+    }
+
+    class SetPrice : Shop
+    {
+        public static Dictionary<Shop.product, int> PriceList = new Dictionary<Shop.product, int>();
+        public void FormPriceList(string catlog)
+        {
+            return 0;
+        }
+
+    }
+    
+}
